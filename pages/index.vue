59fb6e3046c68abc410cb85f656fbec9aba618cd
Dhaval Patel1 <dhaval.patel1@elancoah.com>
2023-01-13 14:16:07 -0500

    firt commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <div class="container">
+  <div class="task" draggable="true">
+    Task 1
+  </div>
+    <div class="task" draggable="true">
+    Task 2
+  </div>
+    <div class="task" draggable="true">
+    Task 3
+  </div>
+</div>
+
+<div class="container">
+  <div class="task" draggable="true">
+    Task 4
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
