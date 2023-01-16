163fc4252274c46bfd58360a790b3a77a5bdeb73
Dhaval Patel <dhaavu@Dhavals-Air.lan>
2023-01-16 16:00:33 -0500

    add Sortable

---
+++ pages/draggable.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+   <div  v-for="element in myArray" :key="element.id">
+        <p class="hello" ref="items">{{element.name}}</p>
+    </div>
+    <button @click="updateNames">Submit</button>
+</div>
+
+</template>
+
+<script>
+import Sortable from 'sortablejs';
+export default {
+    name: "draggable", 
+    data(){
+        return {
+            myArray: [{
+                id:'1', 
+                name:'Dhaval'
+            }, 
+            {
+                id:'2', 
+                name:'Geetu'
+            }, 
+            {
+                id:'3', 
+                name:'Krishang'
+            }]
+        }
+    }, 
+    methods: {
+        updateNames(){
+            this.myArray.push({id:'5', name:'Funny'})
+        }
+    }, 
+    mounted(){
+        console.log(this.$refs.items[0]); 
+      var el =   this.$refs.items[0].parentElement.parentElement; 
+      var sortable = Sortable.create(el);
+
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,57 @@
 <template>
 <div>
-  <div class="container">
-  <div class="task" draggable="true">
+  <div class="gridCointainer">
+      <div class="container" ref="draggableContainer1">
+  <div class="task" >
     Task 1
   </div>
-    <div class="task" draggable="true">
+    <div class="task" >
     Task 2
   </div>
-    <div class="task" draggable="true">
+    <div class="task" >
     Task 3
   </div>
 </div>
 
-<div class="container">
-  <div class="task" draggable="true">
+<div class="container" ref="draggableContainer2">
+  <timeline></timeline> 
+  <div class="timeline">
+  <div class="task" >
     Task 4
   </div>
+  </div>
+
 </div>
+  </div>
+
 </div>
 </template>
 
 <script>
+import Sortable from 'sortablejs';
+import timeline from '../components/timeline.vue';
 export default {
-  name: 'IndexPage'
+  components: { timeline },
+  name: 'IndexPage', 
+  mounted(){
+  var el =   this.$refs.draggableContainer1; 
+  var sortable = Sortable.create(el, {group:"hello"});
+  var el2 =   this.$refs.draggableContainer2; 
+  var sortable = Sortable.create(el2, {group:"hello"});
+  
+  }
 }
 </script>
+
+<style scoped>
+
+.gridCointainer{
+
+  display: grid; 
+  grid-template-columns: 30% 70%;
+
+}
+.timeline{
+  background-color: brown;
+}
+</style>
